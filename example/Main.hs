10ca6dac8b940c572176c21648f3b8cb6ee12f9d
William R. Arellano <arellanowr@gmail.com>
2023-10-06 16:26:28 -0500

    misc: add example

--- example/Main.hs
+++ example/Main.hs
@@ -1,4 +1,19 @@
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeApplications #-}
+
 module Main where
 
+import Data.Csv hiding (decode, decodeByName, decodeByNameWith, decodeWith)
+import Data.Csv.Parser.Megaparsec (decode, decodeByName, decodeByNameWith, decodeWith)
+import Data.Vector (Vector)
+import GHC.Generics (Generic)
+
+data Test = Test {a :: Char, b :: Char, c :: Char} deriving (Eq, Show, Generic)
+
+instance FromRecord Test
+
 main :: IO ()
-main = putStrLn "This is a new example"
+main = do
+  let res = decode @Test NoHeader "example" "a,ba,c\n"
+  print res
